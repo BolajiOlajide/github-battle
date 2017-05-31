468e9885b6e44c1f17b46ee32288fa8c7f74ae96
Bolaji Olajide <bolaji.olajide@andela.com>
2017-06-01 00:54:09 +0100

    display homepage as component

--- app/index.js
+++ app/index.js
@@ -2,17 +2,16 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 require('./index.css');
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                Hello World
-            </div>
-        );
-    }
-}
+import User from './components/User.jsx';
 
 ReactDOM.render(
-    <App />,
+    <User list={[
+        { name: 'Tyler', friend: true },
+        { name: 'Ryan', friend: true },
+        { name: 'Michael', friend: false },
+        { name: 'Mikenzi', friend: false },
+        { name: 'Jessica', friend: true },
+        { name: 'Dan', friend: false }]}
+    />,
     document.getElementById('app')
 );
