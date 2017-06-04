d9689a5b978f01c9ac389899a6d2e1492a0f92c3
Bolaji Olajide <bolaji.olajide@andela.com>
2017-06-04 21:56:58 +0100

    remove hardcoded component

--- app/index.js
+++ app/index.js
@@ -1,17 +1,9 @@
-const React = require('react');
-const ReactDOM = require('react-dom');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
 require('./index.css');
 
-import User from './components/User.jsx';
-
 ReactDOM.render(
-    <User list={[
-        { name: 'Tyler', friend: true },
-        { name: 'Ryan', friend: true },
-        { name: 'Michael', friend: false },
-        { name: 'Mikenzi', friend: false },
-        { name: 'Jessica', friend: true },
-        { name: 'Dan', friend: false }]}
-    />,
+    <App />,
     document.getElementById('app')
 );
