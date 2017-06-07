4d033ac9a15a0cbdb11413701d608fe329c95bb9
Bolaji Olajide <bolaji.olajide@andela.com>
2017-06-08 00:35:50 +0100

    implement routing functionality

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import Popular from './Popular.jsx';
+import Nav from './Nav.jsx';
+import Home from './Home.jsx';
+import Battle from './Battle.jsx';
+
+const Router = BrowserRouter;
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className='container'>
-                <Popular />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <Nav />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/battle' component={Battle} />
+            <Route path='/popular' component={Popular} />
+            <Route render={ () => {
+                return <p> Not Found </p>
+              }} 
+            />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
